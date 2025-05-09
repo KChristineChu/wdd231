50a7f08e13c3ed3cba7832dc85b68b178202849d
KChristineChu <145231764+KChristineChu@users.noreply.github.com>
2025-05-08 23:49:55 -0600

    Worked on html file

---
+++ styles/date.js
@@ -0,0 +1,5 @@
+let myDate = new Date();
+let myYear = myDate.getFullYear();
+document.getElementById("currentyear").textContent = myYear;
+
+document.getElementById("lastModified").textContent = "Last Modification: " + document.lastModified;
